5a527bb61fa48ff90aea786dcb7a91a3609d0327
leonardogazdek <bot.denobot.deno@gmail.com>
2021-01-21 15:41:52 +0100

    Jos malo cruda

--- message.go
+++ message.go
@@ -33,6 +33,301 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 			payload = path
 			break
 		}
+	case "insertBookmark":
+		{
+			var bookmark KnjiznaOznakaUpdate
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &bookmark); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			fmt.Println("bookmark set ", bookmark.Ime, bookmark.Url, bookmark.Kategorije_id, bookmark.Korisnici_id)
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("insert into knjizneoznake (ime, url, korisnici_id, kategorije_id) values(?, ?, ?, ?)")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(bookmark.Ime, bookmark.Url, bookmark.Korisnici_id, bookmark.Kategorije_id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = bookmark
+			break
+		}
+	case "updateBookmark":
+		{
+			var bookmark KnjiznaOznakaUpdate
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &bookmark); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			fmt.Println("bookmark set ", bookmark.Ime, bookmark.Url, bookmark.Kategorije_id, bookmark.Korisnici_id)
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("UPDATE knjizneoznake SET ime = ?, url = ?, kategorije_id = ? WHERE id = ?")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(bookmark.Ime, bookmark.Url, bookmark.Kategorije_id, bookmark.Id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = bookmark
+			break
+		}
+	case "insertExtensions":
+		{
+			var ext ProsirenjeData
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &ext); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("insert into prosirenja (ime, opis) values(?, ?)")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(ext.Ime, ext.Opis)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = ext
+			break
+		}
+	case "updateExtensions":
+		{
+			var ext ProsirenjeData
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &ext); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("UPDATE prosirenja SET ime = ?, opis = ? WHERE id = ?")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(ext.Ime, ext.Opis, ext.Id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = ext
+			break
+		}
+
+	case "insertTab":
+		{
+			var tab OtvoreneKarticeData
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &tab); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("insert into otvorene_kartice (url, korisnici_id) values(?, ?)")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(tab.Url, tab.Korisnici_id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = tab
+			break
+		}
+	case "updateTab":
+		{
+			var tab OtvoreneKarticeData
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &tab); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("UPDATE otvorene_kartice SET url = ? WHERE id = ?")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(tab.Url, tab.Id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = tab
+			break
+		}
+
+	case "insertSettings":
+		{
+			var set PostavkeData
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &set); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("insert into postavke (ime, vrijednost) values(?, ?)")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(set.Ime, set.Vrijednost)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = set
+			break
+		}
+	case "updateSettings":
+		{
+			var set PostavkeData
+			if len(m.Payload) > 0 {
+				// Unmarshal payload
+				if err = json.Unmarshal(m.Payload, &set); err != nil {
+					payload = err.Error()
+					return
+				}
+			}
+
+			db, err := sql.Open("sqlite3", "./resources/app/main.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
+			stmt, err := tx.Prepare("UPDATE postavke SET ime = ?, vrijednost = ? WHERE id = ?")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(set.Ime, set.Vrijednost, set.Id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+
+			payload = set
+			break
+		}
+
 	case "historyNav":
 		{
 			// Unmarshal payload
@@ -128,24 +423,40 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 			}
 			defer db.Close()
 
+			fmt.Println("getting povijest")
 			rows, err := db.Query("select id, url, vremenskistambilj from povijest where korisnici_id = " + action + "")
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			rowsBook, err := db.Query("select p.id,p.ime,p.url,k.ime from knjizneoznake p left join kategorije k on p.kategorije_id = k.id where korisnici_id = " + action + "")
+			fmt.Println("getting knjizneoznake")
+			rowsBook, err := db.Query("select p.id,p.ime,p.url,k.ime,p.kategorije_id from knjizneoznake p left join kategorije k on p.kategorije_id = k.id where korisnici_id = " + action + "")
 			if err != nil {
 				log.Fatal(err)
 			}
-
+			fmt.Println("getting prosirenja")
 			rowsExt, err := db.Query("select p.id, p.ime, p.opis from prosirenja p join korisnici_prosirenja_veza v on p.id = v.prosirenja_id join korisnici k on k.id = v.korisnici_id WHERE v.korisnici_id = " + action + "")
 			if err != nil {
 				log.Fatal(err)
 			}
+
+			fmt.Println("getting otvorene_kartice")
+			rowsTabs, err := db.Query("select id, url, korisnici_id FROM otvorene_kartice WHERE korisnici_id = " + action + "")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("getting postavke")
+			rowsSettings, err := db.Query("select id, ime, vrijednost FROM postavke")
+			if err != nil {
+				log.Fatal(err)
+			}
+
 			defer rows.Close()
 			defer rowsBook.Close()
 			defer rowsExt.Close()
-
+			defer rowsTabs.Close()
+			defer rowsSettings.Close()
 			povijestData := []PovijestData{}
 			for rows.Next() {
 				var id int
@@ -169,15 +480,17 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 				var ime string
 				var url string
 				var kategorija string
-				err = rowsBook.Scan(&id, &ime, &url, &kategorija)
+				var kategorije_id int
+				err = rowsBook.Scan(&id, &ime, &url, &kategorija, &kategorije_id)
 				if err != nil {
 					log.Fatal(err)
 				}
 				knjizneoznakeData = append(knjizneoznakeData, KnjiznaOznakaData{
-					Id:         id,
-					Ime:        ime,
-					Url:        url,
-					Kategorija: kategorija,
+					Id:            id,
+					Ime:           ime,
+					Url:           url,
+					Kategorija:    kategorija,
+					Kategorije_id: kategorije_id,
 				})
 
 			}
@@ -199,6 +512,40 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 
 			}
 
+			otvoreneKarticeData := []OtvoreneKarticeData{}
+			for rowsTabs.Next() {
+				var id int
+				var url string
+				var korisnici_id int
+				err = rowsTabs.Scan(&id, &url, &korisnici_id)
+				if err != nil {
+					log.Fatal(err)
+				}
+				otvoreneKarticeData = append(otvoreneKarticeData, OtvoreneKarticeData{
+					Id:  id,
+					Url: url,
+					Korisnici_id: korisnici_id,
+				})
+
+			}
+
+			postavkeData := []PostavkeData{}
+			for rowsSettings.Next() {
+				var id int
+				var ime string
+				var vrijednost string
+				err = rowsSettings.Scan(&id, &ime, &vrijednost)
+				if err != nil {
+					log.Fatal(err)
+				}
+				postavkeData = append(postavkeData, PostavkeData{
+					Id:  id,
+					Ime: ime,
+					Vrijednost: vrijednost,
+				})
+
+			}
+
 			err = rows.Err()
 			if err != nil {
 				log.Fatal(err)
@@ -211,10 +558,23 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 			if err != nil {
 				log.Fatal(err)
 			}
+
+			err = rowsTabs.Err()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = rowsSettings.Err()
+			if err != nil {
+				log.Fatal(err)
+			}
+
 			arr := KorisnikData{
-				Povijest:      povijestData,
-				KnjizneOznake: knjizneoznakeData,
-				Prosirenja:    prosirenjaData,
+				Povijest:        povijestData,
+				KnjizneOznake:   knjizneoznakeData,
+				Prosirenja:      prosirenjaData,
+				OtvoreneKartice: otvoreneKarticeData,
+				Postavke: 		 postavkeData,
 			}
 			payload = arr
 
@@ -226,9 +586,11 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 }
 
 type KorisnikData struct {
-	Povijest      []PovijestData      `json:"povijestdata"`
-	KnjizneOznake []KnjiznaOznakaData `json:"knjizneoznakedata"`
-	Prosirenja    []ProsirenjeData    `json:"prosirenjadata"`
+	Povijest        []PovijestData        `json:"povijestdata"`
+	KnjizneOznake   []KnjiznaOznakaData   `json:"knjizneoznakedata"`
+	Prosirenja      []ProsirenjeData      `json:"prosirenjadata"`
+	OtvoreneKartice []OtvoreneKarticeData `json:"otvorenekarticedata"`
+	Postavke		[]PostavkeData		  `json:"postavkedata"`
 }
 
 type PovijestData struct {
@@ -238,10 +600,11 @@ type PovijestData struct {
 }
 
 type KnjiznaOznakaData struct {
-	Id         int    `json:"id"`
-	Ime        string `json:"ime"`
-	Url        string `json:"url"`
-	Kategorija string `json:"kategorija"`
+	Id            int    `json:"id"`
+	Ime           string `json:"ime"`
+	Url           string `json:"url"`
+	Kategorija    string `json:"kategorija"`
+	Kategorije_id int    `json:"kategorije_id"`
 }
 
 type ProsirenjeData struct {
@@ -250,8 +613,28 @@ type ProsirenjeData struct {
 	Opis string `json:"opis"`
 }
 
+type OtvoreneKarticeData struct {
+	Id  int    `json:"id"`
+	Url string `json:"url"`
+	Korisnici_id int `json:"korisnici_id"`
+}
+
 type Korisnik struct {
 	Id            int    `json:"id"`
 	Korisnickoime string `json:"korisnickoime"`
 	Datum         string `json:"datum"`
+}
+
+type KnjiznaOznakaUpdate struct {
+	Id            int    `json: "id"`
+	Ime           string `json:"ime"`
+	Url           string `json:"url"`
+	Kategorije_id int    `json:"kategorije_id"`
+	Korisnici_id  int    `json:"korisnici_id"`
+}
+
+type PostavkeData struct {
+	Id int `json:"id"`
+	Ime string `json:"ime"`
+	Vrijednost string `json:"vrijednost"`
 }
--- resources/app/static/js/index.js
+++ resources/app/static/js/index.js
@@ -1,3 +1,4 @@
+let sel_user = 0;
 let index = {
     init: function() {
         // Init
@@ -8,7 +9,6 @@ let index = {
         // Wait for astilectron to be ready
         document.addEventListener('astilectron-ready', function() {
             // Listen
-            index.listen();
         })
     },
     addrInit() {
@@ -20,6 +20,17 @@ let index = {
         // Wait for astilectron to be ready
         document.addEventListener('astilectron-ready', function() {
             index.getUsers();
+            document.body.addEventListener("click", (e) => {
+                if(e.target.className == "form-update") {
+                    var datas = [].filter.call(e.target.attributes, at => /^data-/.test(at.name));
+                    datas = datas.map (el => el.localName.substring(5, el.length))
+                    const form = e.target.parentElement.parentElement.querySelector("form")
+                    datas.map(data => {
+                        form.querySelector(`input[name=${data}]`).value = e.target.getAttribute(`data-${data}`)
+                    })
+                    
+                }
+            })
         })
     },
     getUsers: function() {
@@ -41,6 +52,7 @@ let index = {
             elems.forEach(el => {
                 el.addEventListener('click', function(e) {
                     const id = el.getAttribute('data-id')
+                    sel_user = id
                     // load user
                     let message = {
                         "name": "fetchUserData",
@@ -64,6 +76,9 @@ let index = {
             const elem = document.createElement('div');
             const text = document.createTextNode(hist.url+" - "+hist.vremenskistambilj)
             elem.setAttribute('data-id', hist.id)
+            elem.setAttribute('data-url', hist.url)
+            elem.setAttribute('data-vremenskistambilj', hist.vremenskistambilj)
+            elem.className="form-update"
             elem.appendChild(text)
             document.getElementById("history").appendChild(elem)
         });
@@ -72,6 +87,10 @@ let index = {
             const elem = document.createElement('div');
             const text = document.createTextNode(bk.ime+" - "+bk.url+", kategorija: "+bk.kategorija)
             elem.setAttribute('data-id', bk.id)
+            elem.setAttribute('data-ime', bk.ime)
+            elem.setAttribute('data-url', bk.url)
+            elem.setAttribute('data-kategorije_id', bk.kategorije_id)
+            elem.className="form-update"
             elem.appendChild(text)
             document.getElementById("bookmarks").appendChild(elem)
         });
@@ -80,8 +99,31 @@ let index = {
             const elem = document.createElement('div');
             const text = document.createTextNode(ex.ime+" - "+ex.opis)
             elem.setAttribute('data-id', ex.id)
+            elem.setAttribute('data-ime', ex.ime)
+            elem.setAttribute('data-opis', ex.opis)
+            elem.className="form-update"
             elem.appendChild(text)
             document.getElementById("extensions").appendChild(elem)
+        });
+        data.otvorenekarticedata.map((ex) => {
+            const elem = document.createElement('div');
+            const text = document.createTextNode(ex.url)
+            elem.setAttribute('data-id', ex.id)
+            elem.setAttribute('data-url', ex.url)
+            elem.className="form-update"
+            elem.appendChild(text)
+            document.getElementById("opentabs").appendChild(elem)
+        });
+
+        data.postavkedata.map((ex) => {
+            const elem = document.createElement('div');
+            const text = document.createTextNode(ex.ime+" "+ex.vrijednost)
+            elem.setAttribute('data-id', ex.id)
+            elem.setAttribute('data-ime', ex.ime)
+            elem.setAttribute('data-vrijednost', ex.vrijednost)
+            elem.className="form-update"
+            elem.appendChild(text)
+            document.getElementById("settings").appendChild(elem)
         });
 
     },
@@ -116,10 +158,8 @@ let index = {
         message.payload = (attr == 0) ? "back" : "forward";
 
         // Send message
-        asticode.loader.show();
         astilectron.sendMessage(message, function(message) {
             // Init
-            asticode.loader.hide();
 
             // Check error
             if (message.name === "error") {
@@ -127,7 +167,109 @@ let index = {
                 return
             }
         })
-    }
+    },
+    createBookmark(action) {
+        // Create message
+        let message = {
+            name: (action == 'insert') ? "insertBookmark" : "updateBookmark",
+            payload: {
+                id: parseInt(document.querySelector("#form_bookmarks input[name=id]").value, 10),
+                ime: document.querySelector("#form_bookmarks input[name=ime]").value,
+                url: document.querySelector("#form_bookmarks input[name=url]").value,
+                kategorije_id: parseInt(document.querySelector("#form_bookmarks input[name=kategorije_id]").value, 10),
+                korisnici_id: parseInt(sel_user, 10),
+            },
+        };
+
+        console.log(message)
+
+        // Send message
+        astilectron.sendMessage(message, function(message) {
+            // Check error
+            if (message.name === "error") {
+                asticode.notifier.error(message.payload);
+                return
+            }
+            else {
+                // FEEDBACK OK
+            }
+        })
+    },
+    createExtension(action) {
+        // Create message
+        let message = {
+            name: (action == 'insert') ? "insertExtension" : "updateExtensions",
+            payload: {
+                id: parseInt(document.querySelector("#form_extensions input[name=id]").value, 10),
+                ime: document.querySelector("#form_extensions input[name=ime]").value,
+                opis: document.querySelector("#form_extensions input[name=opis]").value,
+            },
+        };
+
+        console.log(message)
+
+        // Send message
+        astilectron.sendMessage(message, function(message) {
+            // Check error
+            if (message.name === "error") {
+                asticode.notifier.error(message.payload);
+                return
+            }
+            else {
+                // FEEDBACK OK
+            }
+        })
+    },
+    createTab(action) {
+        // Create message
+        let message = {
+            name: (action == 'insert') ? "insertTab" : "updateTab",
+            payload: {
+                id: parseInt(document.querySelector("#form_opentabs input[name=id]").value, 10),
+                url: document.querySelector("#form_opentabs input[name=url]").value,
+                korisnici_id: parseInt(sel_user, 10),
+            },
+        };
+
+        console.log(message)
+
+        // Send message
+        astilectron.sendMessage(message, function(message) {
+            // Check error
+            if (message.name === "error") {
+                asticode.notifier.error(message.payload);
+                return
+            }
+            else {
+                // FEEDBACK OK
+            }
+        })
+    },
+    createSettings(action) {
+        // Create message
+        let message = {
+            name: (action == 'insert') ? "insertSettings" : "updateSettings",
+            payload: {
+                id: parseInt(document.querySelector("#form_settings input[name=id]").value, 10),
+                ime: document.querySelector("#form_settings input[name=ime]").value,
+                vrijednost: document.querySelector("#form_settings input[name=vrijednost]").value,
+            },
+        };
+
+        console.log(message)
+
+        // Send message
+        astilectron.sendMessage(message, function(message) {
+            // Check error
+            if (message.name === "error") {
+                asticode.notifier.error(message.payload);
+                return
+            }
+            else {
+                // FEEDBACK OK
+            }
+        })
+    },
     /*
     listen: function() {
         astilectron.onMessage(function(message) {
